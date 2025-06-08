553f4d25acffdddea363731b472e989120b1e89b
Dennis Ciccone <dennisciccone02@gmail.com>
2025-06-08 20:22:30 +0200

    Creati due oggetti Person nel Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
 
+        PersonBuilder personBuilder = new PersonBuilder();
+        personBuilder.setFirstName("Mario");
+        personBuilder.setLastName("Rossi");
+        personBuilder.setAge(30);
+        personBuilder.setAddress("via Roma 1");
+        Person person1 = personBuilder.build();
+
+        PersonBuilder personBuilder2 = new PersonBuilder();
+        personBuilder2.setFirstName("Luca");
+        personBuilder2.setLastName("Bianchi");
+        personBuilder2.setAge(25);
+        personBuilder2.setAddress("Via Milano 10");
+        Person person2 = personBuilder2.build();
+
+
+        System.out.println(person1);
+        System.out.println(person2);
+
     }
 }
